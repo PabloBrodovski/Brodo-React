06b9f6bfc2785c7ecfac3a28108980191cf02032
“Pablo <fotobrodo@gmail.com>
2024-02-15 00:38:13 -0300

    actualizacion de precios

--- src/componentes/Precios.jsx
+++ src/componentes/Precios.jsx
@@ -19,38 +19,38 @@ const Precios = () => {
             <tbody>
               <tr>
                 <td>polaroid (10x9cm)</td>
-                <td>$350</td>
-                <td>$330</td>
-                <td>$310</td>
-                <td>$290</td>
+                <td>$430</td>
+                <td>$410</td>
+                <td>$390</td>
+                <td>$370</td>
               </tr>
               <tr>
                 <td>10x15cm</td>
-                <td>$500</td>
-                <td>$470</td>
-                <td>$440</td>
-                <td>$410</td>
+                <td>$650</td>
+                <td>$620</td>
+                <td>$590</td>
+                <td>$560</td>
               </tr>
               <tr>
                 <td>13x18cm</td>
-                <td>$900</td>
-                <td>$850</td>
-                <td>$800</td>
-                <td>$750</td>
+                <td>$1.100</td>
+                <td>$1.050</td>
+                <td>$1.000</td>
+                <td>$950</td>
               </tr>
               <tr>
                 <td>15x21cm</td>
-                <td>$1.000</td>
-                <td>$950</td>
-                <td>$900</td>
-                <td>$850</td>
+                <td>$1.250</td>
+                <td>$1.200</td>
+                <td>$1.150</td>
+                <td>$1.100</td>
               </tr>
               <tr>
                 <td>20x30cm</td>
-                <td>$2.000</td>
-                <td>$1.900</td>
-                <td>$1.800</td>
-                <td>$1.750</td>
+                <td>$2.500</td>
+                <td>$2.400</td>
+                <td>$2.300</td>
+                <td>$2.200</td>
               </tr>
             </tbody>
           </table>
@@ -60,7 +60,7 @@ const Precios = () => {
             <tbody>
               <tr>
                 <td>Fotos carnet x4</td>
-                <td>$1.200</td>
+                <td>$1.500</td>
               </tr>
             </tbody>
           </table>
@@ -70,11 +70,11 @@ const Precios = () => {
             <tbody>
               <tr>
                 <td>Básica</td>
-                <td>$1.500</td>
+                <td>$2.000</td>
               </tr>
               <tr>
                 <td>Creación de logo, cambio de fondo, diseño de carteles</td>
-                <td>$3.000</td>
+                <td>$4.000</td>
               </tr>
             </tbody>
           </table>
@@ -84,7 +84,7 @@ const Precios = () => {
             <tbody>
               <tr>
                 <td>Precio por metro cuadrado</td>
-                <td>$15.000</td>
+                <td>$17.000</td>
               </tr>
             </tbody>
           </table>
@@ -94,43 +94,43 @@ const Precios = () => {
             <tbody>
               <tr>
                 <td>50 x 50 cm</td>
-                <td>$8.700</td>
+                <td>$10.250</td>
               </tr>
               <tr>
                 <td>50 x 75 cm</td>
-                <td>$10.600</td>
+                <td>$12.350</td>
               </tr>
               <tr>
                 <td>75 x 75 cm</td>
-                <td>$14.300</td>
+                <td>$16.500</td>
               </tr>
               <tr>
                 <td>75 x 90 cm</td>
-                <td>$16.000</td>
+                <td>$18.400</td>
               </tr>
               <tr>
                 <td>75 x 100 cm</td>
-                <td>$17.200</td>
+                <td>$19.750</td>
               </tr>
               <tr>
                 <td>75 x 120 cm</td>
-                <td>$19.500</td>
+                <td>$22.300</td>
               </tr>
               <tr>
                 <td>100 x 100 cm</td>
-                <td>$22.000</td>
-              </tr>
-              <tr>
-                <td>100 x 120 cm</td>
                 <td>$25.000</td>
               </tr>
               <tr>
+                <td>100 x 120 cm</td>
+                <td>$28.400</td>
+              </tr>
+              <tr>
                 <td>100 x 150 cm</td>
-                <td>$25.500</td>
+                <td>$33.500</td>
               </tr>
               <tr>
                 <td>Banner con porta banner 90 x 190 cm</td>
-                <td>$48.500</td>
+                <td>$59.000</td>
               </tr>
             </tbody>
           </table>
@@ -148,11 +148,11 @@ const Precios = () => {
             </thead>
             <tbody>
               <tr>
+                <td>$700</td>
+                <td>$650</td>
+                <td>$600</td>
+                <td>$550</td>
                 <td>$500</td>
-                <td>$470</td>
-                <td>$440</td>
-                <td>$410</td>
-                <td>$380</td>
               </tr>
             </tbody>
           </table>
@@ -162,7 +162,7 @@ const Precios = () => {
             <tbody>
               <tr>
                 <td>X100</td>
-                <td>$4.800</td>
+                <td>$5.600</td>
               </tr>
             </tbody>
           </table>
@@ -171,58 +171,51 @@ const Precios = () => {
           <table className="table table-striped table-dark">
             <tbody>
               <tr>
-                <td>X50</td>
-                <td>$6.000</td>
-              </tr>
-              <tr>
-                <td>X100</td>
-                <td>$11.000</td>
+                <td>C/U</td>
+                <td>$100</td>
               </tr>
             </tbody>
           </table>
 
           <h2>Impresiones</h2>
+          <h4>Los presios no incluyen edición ni cortes</h4>
           <table className="table table-striped table-dark">
             <tbody>
               <tr>
-                <td>impresiones A4 papel 75g (texto)</td>
-                <td>$80</td>
-              </tr>
-              <tr>
-                <td>impresiones A4 papel 75g (texto y imágenes)</td>
+                <td>Impresiones A4 papel 75g (texto)</td>
                 <td>$150</td>
               </tr>
               <tr>
-                <td>impresiones A4 papel 75g (full color)</td>
-                <td>$200</td>
+                <td>Impresiones A4 papel 75g (imágenes)</td>
+                <td>$300</td>
+              </tr>
+              <tr>
+                <td>Papel ilustración 180g</td>
+                <td>$400</td>
               </tr>
               <tr>
                 <td>Opalina 200g</td>
-                <td>$400</td>
-              </tr>
-              <tr>
-                <td>Ilustración 200g</td>
-                <td>$400</td>
-              </tr>
-              <tr>
-                <td>Glossy 220g (fotográfico)</td>
-                <td>$600</td>
-              </tr>
-              <tr>
-                <td>Adhesivo opaco</td>
                 <td>$500</td>
               </tr>
               <tr>
-                <td>Adhesivo fotográfico</td>
+                <td>Papel adhesivo</td>
+                <td>$600</td>
+              </tr>
+              <tr>
+                <td>Papel fotográfico</td>
                 <td>$700</td>
               </tr>
               <tr>
-                <td>Craft 220g</td>
-                <td>$400</td>
+                <td>Papel adhesivo fotográfico</td>
+                <td>$800</td>
+              </tr>
+              <tr>
+                <td>Papel craft 200g</td>
+                <td>$500</td>
               </tr>
               <tr>
                 <td>Papel para sublimación</td>
-                <td>$350</td>
+                <td>$400</td>
               </tr>
               <tr>
                 <td>Papel Imantado</td>
